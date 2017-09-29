084fac51526215e59dbf5826f0a8b91eab23f19a
Jonny Huang <jonnyhuang@outlook.com>
2017-09-29 12:05:30 +0800

    Site updated: 2017-09-29 12:05:25

--- demo/first-app/register_sw.js
+++ demo/first-app/register_sw.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('sw.js')
     .then(reg => {
       // registration worked
       console.log('[Service Worker] Registration succeeded. Scope is ' + reg.scope);
